5a2807d2a683124058dc0b6846ed5487cd936dd9
Pri Anton Subardio <prianthon@MacBook-Pro-Pri.local>
2016-12-03 22:22:33 +0700

    url get

--- mytweets/tweets/views.py
+++ mytweets/tweets/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    if request.method == 'GET':
+        return HttpResponse('I am called from a get Request')
+    elif request.method == 'POST':
+        return HttpResponse('I am called from a post Request')
