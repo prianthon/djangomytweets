1fef3e2b3b7740536a3d9ec07f9dbb762865642b
prianthon <prianthonsubardio@gmail.com>
2016-12-15 10:45:33 +0700

    static nav

--- mytweets/tweets/views.py
+++ mytweets/tweets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View
+from models import User, Tweet
 
 # Create your views here.
 class Index(View):
