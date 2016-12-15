54ac4d265047a6b5c5a12df7f827db0effa2cede
prianthon <prianthonsubardio@gmail.com>
2016-12-15 08:30:50 +0700

    bs move

--- mytweets/mytweets/urls.py
+++ mytweets/mytweets/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mytweets.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', Index.as_view()),
+    url(r'^user/(\w+)/$', Profile.as_view()),
+    url(r'^admin/', include(admin.site.urls)),
 )
